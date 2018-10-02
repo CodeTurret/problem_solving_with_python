1b03cb309f636f5b539e519efdf438ea6e2b2da3
CodeTurret <mushfiq.fuad@gmail.com>
2018-10-03 01:05:26 +0600

    nth fibonacci number using memozation

---
+++ fibonacci_sequence_memoization.py
@@ -0,0 +1,24 @@
+
+#create a dictonary
+
+fibonacci_cache = {}
+
+def fibonacci(n):
+    #if we have cached the value, then return it
+    if n in fibonacci_cache:
+        return fibonacci_cache[n]
+
+    #if we don't have cached value, then compute the nth term
+    if n == 1:
+        value = 1
+    elif n == 2:
+        value = 1
+    elif n > 2:
+        value = fibonacci(n-1) + fibonacci (n-2)
+
+    # Cache the value and return it
+    fibonacci_cache[n] = value
+    return value
+
+for n in range(1, 101):
+    print(n, ":", fibonacci(n))
